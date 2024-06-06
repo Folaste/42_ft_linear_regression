7700f309361886dcfcbad30be7df457a08ff595f
Folaste <41912691+Folaste@users.noreply.github.com>
2024-06-06 19:08:50 +0200

    Adding error function for bonus

--- train.py
+++ train.py
@@ -3,6 +3,17 @@ import os.path
 from estimate_price import estimate_price
 
 
+def mean_squared_error(mileage, price, theta0, theta1):
+    error_sum = 0
+    for i in range(len(mileage)):
+        error_sum += (estimate_price(mileage[i], theta0, theta1) - price[i]) ** 2
+    return error_sum / (2 * len(mileage))
+
+
+def gradient_descent(mileage, price, theta0, theta1, learning_rate):
+    pass
+
+
 def main():
     pass
 
